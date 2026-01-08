478cf031323cab1d94061d2f040f18ec0fab415c
With-chomok <dipoldas182@gmail.com>
2026-01-08 15:41:19 +0600

    problem 7 solved

--- index.js
+++ index.js
@@ -12,22 +12,35 @@ function removeDuplicates(arr) {
 }
 
 const arrayWithDuplicates = [1, 2, 2, 3, 4, 4];
-// console.log(removeDuplicates(arrayWithDuplicates));
+console.log("Removes All Duplicate Numbers:",removeDuplicates(arrayWithDuplicates));
 
 // Problem 6: Sum of All Numbers in an Array
 // solved below:
 
-function sumOfArr (arr) {
-    let sum = 0;
-    for (let nums of arr){
-        console.log(nums);
-        sum += nums;
-    }
-    return sum;
+function sumOfArr(arr) {
+  let sum = 0;
+  for (let nums of arr) {
+    sum += nums;
+  }
+  return sum;
 }
 
 const arrayToSum = [1, 2, 3, 4];
-console.log("Total Num:",sumOfArr(arrayToSum));
+console.log("Total Num:", sumOfArr(arrayToSum));
 
+// Problem 7: Find Even Numbers in an Array
+// [1, 2, 3, 4, 5, 6]
 
+function giveEvenNumbs(arr) {
+  let evenNumbs = [];
+  for (let numbs of arr) {
+    if (numbs % 2 === 0) {
+      evenNumbs.push(numbs);
+    }
+  }
+  return evenNumbs;
+}
 
+const arrayForEvenNumbs = [1, 2, 3, 4, 5, 6];
+const giveEvenNumbsResult = giveEvenNumbs(arrayForEvenNumbs);
+console.log("All Even Number Are: ", giveEvenNumbsResult);
